603929ad326911a380743e58f6296ec357975e9e
AmandaFt07 <amandafuruta@gmail.com>
2019-09-18 11:20:12 -0300

    configuração dos movimentos com tecla

--- main.c
+++ main.c
@@ -18,6 +18,7 @@ Data 28/08/19
 int main(){
     char matrix[ROWS][COLUMNS];
     int posI, posJ;
+    int keypressed=0;
     
 
     posI= 0; //linha
@@ -31,7 +32,7 @@ int main(){
     system ("cls");
 
     //animação do jogo
-    while (1) {
+    while ( keypressed != ESC) { //27 é o cod do esc- para sair do jogo
         gotoxy(0,0);
         
         matrix[posI][posJ]= '@';
@@ -41,7 +42,29 @@ int main(){
 
         matrix[posI][posJ]= ' ';
         if (posI<ROWS-1) posI++;
+
+
+
+        //lendo teclas- kbhit -> da biblio conio.h
+        keypressed=0; //para o bichinho não ficar indo para o lado infinitamente
+        if(kbhit()){ //quando alguma tecla é apertada, o kbhit se torna verdadeiro 
+            keypressed = getch(); //o keypressed vai receber qual tecla foi apertada
+        }
+
+        if (keypressed==ARROWS){ //224 é o codigo do teclado de setas e não alfabeto
+            keypressed=getch();//vai ler qual seta foi acionado
+        } 
         
+        switch (keypressed){
+            case TECLA_A: //caso apertar o 'a' vai para esquerda tbm
+            case LEFT://codigo da seta para esquerda 
+                if( posJ>0) posJ--; 
+                    break; 
+            case TECLA_D: // caso apertar 'd' vai para direita
+            case RIGHT: //codigo de seta para direita
+                if (posJ< COLUMNS-1) posJ++;
+                    break; 
+        }
 
     } 
 
--- tetris.h
+++ tetris.h
@@ -8,10 +8,19 @@ Data 28/08/19
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <conio.h>
 
 //Config das dimensões da matriz principal do jogo
 #define COLUMNS 60
 #define ROWS 20
+
+//configuração das teclas
+#define ESC 27
+#define ARROWS 224 //teclado de setas
+#define LEFT 75
+#define RIGHT 77
+#define TECLA_A 97
+#define TECLA_D 100
 
 //assinaturas
 void init (char matrix[ROWS][COLUMNS]);
